d6d99d53c761c866d8acc2c525f4ea7d6369ddd9
Can Çetin <swartzcan@gmail.com>
2023-11-09 18:28:32 +0300

    moving channel update

--- index.js
+++ index.js
@@ -16,8 +16,24 @@ const client = new Client({
 const REST_API_URL = "http://lidoma-new.local/wp-json/appforms/v1/form-data";
 const UPDATE_API_URL =
   "http://lidoma-new.local/wp-json/appforms/v1/update-form";
-const CHANNEL_ID = "1172167876417958018";
+const CHANNEL_ID = "1172185965624836176";
+const APPROVED_CHANNEL_ID = "1172184660441313432";
+const REJECTED_CHANNEL_ID = "1172184691722432553";
 let lastFormTime = "";
+
+async function fetchFormStatus(submissionId) {
+  const url = `${REST_API_URL}/${submissionId}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching form status:", error);
+    return null; // Hata durumunda null dön
+  }
+}
 
 async function fetchFromWordPressAPI(url) {
   try {
@@ -98,62 +114,51 @@ function sendToDiscord(submission) {
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isButton()) return;
-
   const [action, submissionId] = interaction.customId.split("_");
+  const originalMessage = interaction.message;
 
   if (action === "approve") {
     await approveSubmission(submissionId);
-    if (interaction.deferred || interaction.replied) {
-      await interaction.followUp({
-        content: "Form approved!",
-        components: [],
-        ephemeral: true
-      });
-    } else {
-      await interaction.reply({
-        content: "Form approved!",
-        components: [],
-        ephemeral: true
-      });
-    }
+    await moveAndDeleteMessage(originalMessage, APPROVED_CHANNEL_ID);
+    // Kullanıcıya bildirim gönder
+    await interaction.reply({
+      content: "Form approved and moved!",
+      ephemeral: true
+    });
   } else if (action === "reject") {
     await rejectSubmission(submissionId);
-    if (interaction.deferred || interaction.replied) {
-      await interaction.followUp({
-        content: "Form rejected!",
-        components: [],
-        ephemeral: true
-      });
-    } else {
-      await interaction.reply({
-        content: "Form rejected!",
-        components: [],
-        ephemeral: true
-      });
-    }
+    await moveAndDeleteMessage(originalMessage, REJECTED_CHANNEL_ID);
+    // Kullanıcıya bildirim gönder
+    await interaction.reply({
+      content: "Form rejected and moved!",
+      ephemeral: true
+    });
   }
 });
 
+
 //bu kısıma bakacağım
 /*client.on('interactionCreate', async interaction => {
-    if (!interaction.isButton()) return;
+  if (!interaction.isButton()) return;
 
-    const [action, submissionId] = interaction.customId.split('_');
-    const statusField = interaction.message.embeds[0].fields.find(field => field.name === 'Status');
+  await interaction.deferReply({ ephemeral: true });
 
-    if (action === 'approve') {
-        statusField.value = 'Approved';
-    } else if (action === 'reject') {
-        statusField.value = 'Rejected';
-    }
+  const [action, submissionId] = interaction.customId.split('_');
+  const statusField = interaction.message.embeds[0].fields.find(field => field.name === 'Status');
 
-    const updatedEmbed = new MessageEmbed(interaction.message.embeds[0])
-        .setFields(interaction.message.embeds[0].fields);
+  if (action === 'approve') {
+    statusField.value = 'Approved';
+  } else if (action === 'reject') {
+    statusField.value = 'Rejected';
+  }
 
-    await interaction.reply({ content: `Application updated as ${statusField.value}.`, ephemeral: true });
+  const updatedEmbed = new MessageEmbed(interaction.message.embeds[0])
+    .setFields(interaction.message.embeds[0].fields);
 
-    await interaction.message.edit({ embeds: [updatedEmbed] });
+  await interaction.message.edit({ embeds: [updatedEmbed] });
+  await interaction.followUp({ content: `Application updated as ${statusField.value}.`, ephemeral: true });
 });*/
+
 
 async function approveSubmission(submissionId) {
   try {
@@ -194,6 +199,18 @@ async function rejectSubmission(submissionId) {
     console.error("Error rejecting form:", error);
   }
 }
+
+async function moveAndDeleteMessage(originalMessage, targetChannelId) {
+  const channel = client.channels.cache.get(targetChannelId);
+  if (channel) {
+    // Mesajı yeni kanala taşı
+    channel.send({ embeds: originalMessage.embeds, components: [] });
+
+    // Orijinal mesajı sil
+    originalMessage.delete().catch(console.error);
+  }
+}
+
 
 client.login(process.env.CLIENT_BOT_TOKEN
 );
